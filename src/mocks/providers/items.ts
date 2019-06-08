f7d1725f3f24d837db0ae750e49e00151462a1b9
Quevin Jagmohandas <quevin.happycode@gmail.com>
2019-06-08 10:00:57 +0100

    update 3

--- src/mocks/providers/items.ts
+++ src/mocks/providers/items.ts
@@ -16,39 +16,39 @@ export class Items {
   constructor() {
     let items = [
       {
-        "name": "Burt Bear",
-        "profilePic": "assets/img/speakers/bear.jpg",
-        "about": "Burt is a Bear."
+        "name": "Tomato",
+        "profilePic": "assets/img/speakers/Tomato.jpg",
+        "about": ""
       },
       {
-        "name": "Charlie Cheetah",
-        "profilePic": "assets/img/speakers/cheetah.jpg",
-        "about": "Charlie is a Cheetah."
+        "name": "Carrot",
+        "profilePic": "assets/img/speakers/Carrot.jpg",
+        "about": ""
       },
       {
-        "name": "Donald Duck",
-        "profilePic": "assets/img/speakers/duck.jpg",
-        "about": "Donald is a Duck."
+        "name": "Bread",
+        "profilePic": "assets/img/speakers/Bread.jpg",
+        "about": ""
       },
       {
-        "name": "Eva Eagle",
-        "profilePic": "assets/img/speakers/eagle.jpg",
-        "about": "Eva is an Eagle."
+        "name": "Cholesterol Medicine",
+        "profilePic": "assets/img/speakers/Cholesterol-pills.jpg",
+        "about": ""
       },
       {
-        "name": "Ellie Elephant",
-        "profilePic": "assets/img/speakers/elephant.jpg",
-        "about": "Ellie is an Elephant."
+        "name": "Dog food",
+        "profilePic": "assets/img/speakers/Dog food.jpg",
+        "about": ""
       },
       {
-        "name": "Molly Mouse",
-        "profilePic": "assets/img/speakers/mouse.jpg",
-        "about": "Molly is a Mouse."
+        "name": "Cheese",
+        "profilePic": "assets/img/speakers/Cheese.jpg",
+        "about": ""
       },
       {
-        "name": "Paul Puppy",
-        "profilePic": "assets/img/speakers/puppy.jpg",
-        "about": "Paul is a Puppy."
+        "name": "Soup",
+        "profilePic": "assets/img/speakers/Soup.jpg",
+        "about": ""
       }
     ];
 
